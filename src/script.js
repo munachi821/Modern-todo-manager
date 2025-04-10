0a058a24184c289ed31209d899bf11d093c5ce53
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-04-10 15:08:40 +0100

    preetier ignore
    made preetier to ignore my javascript

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const notification = document.getElementById("notification");
 
-const notificationBtn = document.getElementById("notificationbtn").addEventListener("click", () => {
+const notificationBtn = document
+  .getElementById("notificationbtn")
+  .addEventListener("click", () => {
     notification.classList.remove("opacity-0", "scale-0", "left-[-350px]");
     notification.classList.add("left-4");
 });
